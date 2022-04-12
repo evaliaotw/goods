1277e2b67ee069701ba060b6d7557183aad92deb
evaliaotw <evaliaotw@gmail.com>
2022-04-13 01:29:15 +0800

    txt to excel

--- products.py
+++ products.py
@@ -4,7 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
-	p = []
+	#p = []
 	#p.append(name)
 	#p.append(price)
 	p = [name, price]
@@ -14,3 +14,8 @@ print(products[0][0], '的價格是', products[0][1])
 
 for p in products:
 	print(p[0],'的價格是', p[1])
+
+with open('products.txt', 'w', encoding='utf-8') as f:
+	f.write('商品' + ',' + '價格' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
